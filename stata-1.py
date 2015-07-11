33b2c2887fe48b8c8bf4ccfcae652d9778782789
T_Shtirlits <tshtirlits@gmail.com>
2015-07-12 01:40:20 +0500

    Заработал пример работы с Grab и Lxml :) ура!

--- stata-1.py
+++ stata-1.py
@@ -1,3 +1,29 @@
-#!/usr/bin/python3.4
-print ('assa!');
+#!/usr/bin/python
+import lxml.html as html
+from pandas import DataFrame
+
+main_domain_stat = 'http://worldofwarships.ru/community/accounts/'
+
+nick = ('61510-T_Shtirlits','163480-')
+
+print (nick);
+player_url=main_domain_stat+nick[1]+'/#!general_pvp'
+print (player_url)
+page = html.parse(player_url)
+
+e = page.getroot().\
+        find_class('avg_xp').\
+        pop()
+print (e)
+#t = e.getchildren().pop()
+
+#events_tabl = DataFrame([{'EVENT':i[0].text, 'LINK':i[2]} for i in t.iterlinks()][5:])
+
+#event_date = DataFrame([{'EVENT': evt.getchildren()[0].text_content(), 'DATE':evt.getchildren()[1].text_content()} for evt in t][2:])
+
+#sum_event_link = events_tabl.set_index('EVENT').join(event_date.set_index('EVENT')).reset_index()
+
+#sum_event_link.to_csv('..\list_ufc_events.csv',';',index=False)
+
+
 
--- test_grab.py
+++ test_grab.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python
+# -*- coding: UTF-8 -*-
 from grab import Grab
+#from BeautifulSoup import BeautifulSoup
+import html5lib
 
-g = Grab()
-resp = g.go('http://ya.ru')
-print(resp.body)
+g = Grab('http://worldofwarships.ru/community/accounts/61510-T_Shtirlits/')
+resp = g.go('http://worldofwarships.ru/community/accounts/61510-T_Shtirlits/')
+#print(resp.body)
+#print g.xpath_text(".//*[@id='general_pvp']/div[2]/div/div/div[1]/table/tbody/tr[2]/td[2]/span")
+print g.doc.select(".//*[@id='general_pvp']/div[2]/div/div/div[1]/table/tbody/tr[2]/td[2]/span").text()
+#print g.xpath('//div[@id="error"]').text_content()
 
+#for elem in g.xpath_list('//h3'):
+#    print elem.text
+
+#print g.doc.tree.xpath_text(".//*[@id='general_pvp']/div[2]/div/div/div[1]/table/tbody/tr[2]/td[2]/span")
+print ("Количество боев: ") + g.doc.select(".//*[@id='general_pvp']/div[2]/div/div/div[1]/table/tbody/tr[1]/td[2]/span").text()
